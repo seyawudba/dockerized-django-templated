2bd66cc17884e6a258e5252be96f669f17d486c2
Seyram Awudi <seyawudb@gmail.com>
2024-07-02 14:53:26 +0000

    An html templated django app.

--- suapon/schools/urls.py
+++ suapon/schools/urls.py
@@ -17,14 +17,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from schools import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
-    path('school',include('schools.urls'))
-
-]
+    path('',views.display_web)
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- suapon/schools/views.py
+++ suapon/schools/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def display_web(request):
+    return render(request,'child.html')
